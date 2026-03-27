8ce52ffcdfd7fe29d1af548fe998bf0dbbd4c319
climvarufrn <climvar.ufrn@gmail.com>
2026-03-27 09:30:13 -0300

    Deploy user site

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite"
 import path from "path"
 
 export default defineConfig({
-  base: "/climvar.github.io/",
+  base: "/", // 🔥 AGORA É ROOT
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
